d2c9d4fb1c303ee1e0babcf3eeb0d74138c3ca5e
Jude Monie <judemonietv@gmail.com>
2025-11-01 12:41:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Bot code will go here
+const { Telegraf } = require('telegraf');
+
+// YOUR TELEGRAM BOT TOKEN
+const BOT_TOKEN = '8230608492:AAEy8bW21yidejrjgsXHB1H_fwHTk6YdjlQ'; 
+
+const bot = new Telegraf(BOT_TOKEN);
+
+// Start command sends a welcome message
+bot.start((ctx) => ctx.reply('Welcome! I am connected to a database.'));
+
+// Launch the bot to start listening for updates
+bot.launch();
